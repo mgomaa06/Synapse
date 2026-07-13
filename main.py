91ececb88aa3781047743ed24657693428f70fd9
Plagg06 <90144944+Plagg06@users.noreply.github.com>
2026-07-13 20:41:53 +0300

    basic FastAPI server added

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+from fastapi import FastAPI, UploadFile
+from PIL import Image
+
 def main():
     print("Hello from synapse-mentorship-summer-2026!")
 
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    image = Image.open(file.file)
+    width, height = image.size
+    return {"filename": file.filename,
+            "height": height,
+            "width": width}
 
 if __name__ == "__main__":
     main()
