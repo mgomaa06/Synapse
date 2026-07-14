960c0504d60ada16d1519096ce7b4aa003a4cbcb
Plagg06 <90144944+Plagg06@users.noreply.github.com>
2026-07-14 03:23:38 +0300

    Added InferenceResponse and ImageMetadata response models

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 from fastapi import FastAPI, UploadFile
 from PIL import Image
+from pydantic import BaseModel
 
 def main():
     print("Hello from synapse-mentorship-summer-2026!")
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+class InferenceResponse(BaseModel):
+    filename: str
+    prediction: str
+    confidence: float
+
+class ImageMetadata(BaseModel):
+    filename: str
+    height: int
+    width: int
+
 @app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    image = Image.open(file.file)
-    width, height = image.size
-    return {"filename": file.filename,
-            "height": height,
-            "width": width}
+async def create_upload_file(file: UploadFile, response = ImageMetadata):
+    if file.content_type == "image/jpeg" or file.content_type == "image/png":
+        image = Image.open(file.file)
+        width, height = image.size
+        return {"filename": file.filename,
+                "height": height,
+                "width": width}
+    else:
+        return "invalid file type"
+
 
 if __name__ == "__main__":
     main()
